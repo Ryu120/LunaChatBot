5a42703f5f5cc3b838dfd72a7bb9a9a6f88e4b65
Ryu120 <90622022+Ryu120@users.noreply.github.com>
2021-10-04 08:27:15 +0530

    Update luna.py

--- luna.py
+++ luna.py
@@ -105,7 +105,7 @@ async def main():
     session = ClientSession()
     arq = ARQ(ARQ_API_BASE_URL, ARQ_API_KEY, session)
 
-    await Maki.start()
+    await luna.start()
     print(
         """
 -----------------
